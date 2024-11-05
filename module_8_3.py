803881e9f91e815fec1124284ec636b8294d791e
Ilya-23 <ilya.gawrin.7635@gmail.com>
2024-11-05 21:32:06 +0700

    Add files via upload

---
+++ module_8_3.py
@@ -0,0 +1,71 @@
+class IncorrectVinNumber(Exception):
+    def __init__(self, message):
+        self.message = message
+
+class IncorrectCarNumbers(Exception):
+    def __init__(self, message):
+        self.message = message
+
+class Car:
+    def __init__(self, model, __vin, __numbers):
+        self.model = model
+        self.__vin = __vin
+        self.__numbers = __numbers
+        Car.__is_valid_vin(self, __vin)
+        Car.__is_valid_numbers(self, __numbers)
+
+    def __is_valid_vin(self, vin_number):
+        check1 = False
+        if isinstance(vin_number, int) is True:
+            if 1000000 <= vin_number <= 9999999:
+                check1 = True
+            else:
+                raise IncorrectVinNumber('Неверный диапазон для vin номера')
+        else:
+            raise IncorrectVinNumber('Некорректный тип vin номер')
+        return check1
+
+    def __is_valid_numbers(self, numbers):
+        check2 = False
+        if isinstance(numbers, str) is True:
+            if len(numbers) == 6:
+                check2 = True
+            else:
+                raise IncorrectCarNumbers('Неверная длина номера')
+        else:
+            raise IncorrectCarNumbers('Некорректный тип данных для номеров')
+        return check2
+
+try:
+  first = Car('Model1', 1000000, 'f123dj')
+except IncorrectVinNumber as exc:
+  print(exc.message)
+except IncorrectCarNumbers as exc:
+  print(exc.message)
+else:
+  print(f'{first.model} успешно создан')
+
+try:
+  second = Car('Model2', 300, 'т001тр')
+except IncorrectVinNumber as exc:
+  print(exc.message)
+except IncorrectCarNumbers as exc:
+  print(exc.message)
+else:
+  print(f'{second.model} успешно создан')
+
+try:
+  third = Car('Model3', 2020202, 'нет номера')
+except IncorrectVinNumber as exc:
+  print(exc.message)
+except IncorrectCarNumbers as exc:
+  print(exc.message)
+else:
+  print(f'{third.model} успешно создан')
+
+
+
+
+
+
+
